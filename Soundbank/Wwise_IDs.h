9984730633661ab70dbd30ec6f2fdcde29a060e5
josemorante <107500711+josemorante@users.noreply.github.com>
2025-04-16 17:34:45 -0400

    jj
    kk

---
+++ Soundbank/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID NEW_EVENT = 3050945240U;
+        static const AkUniqueID PAUSEGAME = 1589270263U;
+        static const AkUniqueID PLAY_AMBIENCE_BLEND = 3849550482U;
+        static const AkUniqueID PLAY_FIREPLACE = 1501216439U;
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_UNICORN = 403259930U;
+        static const AkUniqueID PLAY_UNICORN_01 = 347293528U;
+        static const AkUniqueID PLAYENGINE = 737062081U;
+        static const AkUniqueID RELEASE = 2405648188U;
+        static const AkUniqueID RESET = 4017455162U;
+        static const AkUniqueID RESUMEGAME = 1133184122U;
+        static const AkUniqueID STOP_AMBIENCE_BLEND = 3433239432U;
+        static const AkUniqueID STOPENGINE = 3762828835U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace CONDICION
+        {
+            static const AkUniqueID GROUP = 4065764457U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GANAR = 3575068712U;
+                static const AkUniqueID PERDER = 2433235219U;
+            } // namespace SWITCH
+        } // namespace CONDICION
+
+        namespace FOOTSTEP_SWITCHGROUP
+        {
+            static const AkUniqueID GROUP = 2116499235U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEP_SWITCHGROUP
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TIMEOFTHEDAY = 792248456U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
